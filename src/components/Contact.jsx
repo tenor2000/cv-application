6f2dd527dfc12d8d472a0f0361c772107c26e5a2
tenor2000 <tenor2000@gmail.com>
2024-05-06 10:36:24 -0400

    set up files system

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+
+export const ContactComp = () => {
+  return (
+  <div>
+      <h1>Test title</h1>
+      <svg>
+          <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+          <input type="text" />
+      </form>
+  </div>
+  )
+}
+
